93810e6257c3873966dafe78a2f4f596fafc7335
Kenji Yasumitsu <kenjiyasumitsu@gmail.com>
2024-09-17 02:19:53 +0900

    init

---
+++ InventoryItem/Kompositum58Shipment.lua
@@ -0,0 +1,20 @@
+UndefineClass('Kompositum58Shipment')
+DefineClass.Kompositum58Shipment = {
+	__parents = { "ValuableItemContainer" },
+	__generated_by_class = "ModItemInventoryItemCompositeDef",
+
+
+	object_class = "ValuableItemContainer",
+	Repairable = false,
+	Icon = "Mod/DiamondBEnhanced/Images/18557-128x128x32",
+	SubIcon = "UI/Icons/Items/kompositum58",
+	DisplayName = T(786856547334, --[[ModItemInventoryItemCompositeDef Kompositum58Shipment DisplayName]] "Kompositum Shipment"),
+	DisplayNamePlural = T(730786364203, --[[ModItemInventoryItemCompositeDef Kompositum58Shipment DisplayNamePlural]] "Kompositum Shipments"),
+	Description = T(899423254887, --[[ModItemInventoryItemCompositeDef Kompositum58Shipment Description]] "A shipment of Kompositum recovered from the enemy."),
+	AdditionalHint = T(339309137675, --[[ModItemInventoryItemCompositeDef Kompositum58Shipment AdditionalHint]] "<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> <color 130 128 120>Can be opened to receive the items</color>"),
+	Valuable = 1,
+	Cost = 10000,
+	loot_def = "Kompositum58Shipment",
+	RestockWeight = 0,
+}
+
